a712540953148afe1571a75bc79c16838efe0916
Aviral Verma <bt20cse214@iiitn.ac.in>
2022-04-21 23:50:46 +0530

    Fix: Navbar, Links, Added: Particle.js, TODO: Footer, Alignment

--- Homepage/index.php
+++ Homepage/index.php
@@ -1,132 +1,156 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <!--favicon-->
-        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
-        <link rel="icon" href="favicon.ico" type="image/icon">
-        <title>IIITN Placement Portal</title>
-        <!-- Bootstrap Core CSS -->
-        <!-- <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"> -->
-        <!-- Footer -->
-        <link type="text/css" rel="stylesheet" href="css/Navbar.css">
-        <!-- Custom Fonts -->
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
-        rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
-        rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
-        <!-- Plugin CSS -->
-        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
-        <!-- Custom CSS -->
-        <link rel="stylesheet" href="css/creative.css" type="text/css">
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!--favicon-->
+  <link rel="shortcut icon" href="/TnP-Portal-IIITN/Homepage/images/newLogo.png" type="image/icon">
+  <link rel="icon" href="/TnP-Portal-IIITN/Homepage/images/newLogo.png" type="image/icon">
+  <title>IIITN Placement Portal</title>
+  <!-- Bootstrap Core CSS -->
+  <!-- <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"> -->
+  <!-- Footer -->
+  <link type="text/css" rel="stylesheet" href="css/Navbar.css">
+  <!-- Custom Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+  <!-- Plugin CSS -->
+  <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/creative.css" type="text/css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-    </head>
+</head>
 
-    <body id="page-top">
-	<div class="parent-container">
-      <div class="navbar-container">
-        <nav class="navbar">
-          <div class="nav-logo">
-            <div class="iiitn-logo">
-              <Link to="/">
-                <img src="/TnP-Portal-IIITN/Homepage/images/newLogo.png" alt="iiitn-logo" />
-              </Link>
-            </div>
-            <div class="nav-title">
-              <div class="logo-text">
-                <h1>Indian Institute of Information Technology, Nagpur</h1>
-              </div>
-              <div class="tagline">
-                <h2>
-                  An Institution of National Importance By An Act of Parliament
-                </h2>
-              </div>
-            </div>
-    
+<body id="page-top">
+  <div class="parent-container">
+    <div class="navbar-container">
+      <nav class="navbar">
+        <div class="nav-logo">
+          <div class="iiitn-logo">
+            <Link to="/">
+            <img src="/TnP-Portal-IIITN/Homepage/images/newLogo.png" alt="iiitn-logo" />
+            </Link>
           </div>
-        </nav>
+          <div class="nav-title">
+            <div class="logo-text">
+              <h1>Indian Institute of Information Technology, Nagpur</h1>
+            </div>
+            <div class="tagline">
+              <h2>
+                An Institution of National Importance By An Act of Parliament
+              </h2>
+            </div>
+          </div>
+
+        </div>
+      </nav>
+    </div>
+  </div>
+  <hr class="nav-divider" />
+
+  <div id="mainNav">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="bottom-nav">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a class="page-scroll" href="../SProfile/index.php">Student Login</a>
+          </li>
+          <li>
+            <a class="page-scroll" href="../HODProfile/index.php">HOD Login</a>
+          </li>
+          <li>
+            <a class="page-scroll" href="../PProfile/index.php">Placement Login</a>
+          </li>
+          <li>
+            <a class="page-scroll" href="../PriProfile/index.php">Administrative Login</a>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </div>
+  <header>
+    <div id="particles-js" class="header-content heading">
+      <div class="header-content-inner">
+        <script src="/TnP-Portal-IIITN/particles.js-master/particles.js"></script>
+        <script src="/TnP-Portal-IIITN/particles.js-master/demo/js/app.js"></script>
+        <h1>Placement Management System</h1>
+        <hr>
+        <p>We are here to Build your Skills and Career with our Driven Passion and Reality.</br>Click Below to Get Our Current Drive
+          Details</p>
+        <a href="../Drives/index.php" class="btn btn-primary btn-xl page-scroll">Current Drives</a>
       </div>
     </div>
-    <hr class="nav-divider" />
-         
-        <header>
-            <div class="header-content">
-                <div class="header-content-inner">
-                    <h1>PLACEMENT MANAGEMENT SYSTEM</h1>
-                    <hr>
-                    <p>We are here to Build your Skills and Career with our Driven Passion and Reality.</br>Click Below to Get Our Current Drive
-                        Details</p>
-                    <a href="../Drives/index.php" class="btn btn-primary btn-xl page-scroll">Current Drives</a>
-                </div>
-            </div>
-        </header>
-        <div class="footer">
-			<div class="container">
-				<div class="col-md-3 ftr_navi ftr">
-					<h3>NAVIGATION</h3>
-					<ul>
-						<li>
-							<a href="../TnP-Portal-IIITN/TnP-Portal-IIITN/Homepage/index.php">Home</a>
-						</li>
-						<li>
-							<a href="../SProfile/index.php">Student Login</a>
-						</li>
-						<li>
-							<a href="../PProfile/index.php">Placement Login</a>
-						</li>
-						<li>
-							<a href="../HODProfile/index.php">HOD Login</a>
-						</li>
-						<li>
-							<a href="../Priprofile/index.php">Administrative Login</a>
-						</li>
-					</ul>
-				</div>
-				<div class="col-md-3 ftr_navi ftr">
-					<h3>MEMBERS</h3>
-					<ul>
-						<li>
-							<a href="#">Customer Support</a>
-						</li>
-						<li>
-							<a href="#">Placement Support</a>
-						</li>
-						<li>
-							<a href="#">Faculty Support</a>
-						</li>
-						<li>
-							<a href="#">Registered Companies</a>
-						</li>
-						<li>
-							<a href="#">Training</a>
-						</li>
-					</ul>
-				</div>
-				<div class="col-md-3 get_in_touch ftr">
-					<h3>GET IN TOUCH</h3>
-					<p>University Hills</p>
-					<p>Kalamessery, Kochi, India</p>
-					<p>83019-79790/221456</p>
-					<a href="mailto:soumen1696@gmail.com">Soumen1696@gmail.com</a>
-				</div>
-				<div class="col-md-3 ftr-logo">
-					<p>Copyright &copy; 2018 CUSAT-SOE | Developed by
-              <a href="http://znumerique.azurewebsites.net" target="_parent">HyperMine Technologies</a>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
-    </body>
+  </header>
+  <div class="footer">
+    <div class="container">
+      <div class="col-md-3 ftr_navi ftr">
+        <h3>NAVIGATION</h3>
+        <ul>
+          <li>
+            <a href="../TnP-Portal-IIITN/TnP-Portal-IIITN/Homepage/index.php">Home</a>
+          </li>
+          <li>
+            <a href="../SProfile/index.php">Student Login</a>
+          </li>
+          <li>
+            <a href="../PProfile/index.php">Placement Login</a>
+          </li>
+          <li>
+            <a href="../HODProfile/index.php">HOD Login</a>
+          </li>
+          <li>
+            <a href="../Priprofile/index.php">Administrative Login</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-3 ftr_navi ftr">
+        <h3>MEMBERS</h3>
+        <ul>
+          <li>
+            <a href="#">Customer Support</a>
+          </li>
+          <li>
+            <a href="#">Placement Support</a>
+          </li>
+          <li>
+            <a href="#">Faculty Support</a>
+          </li>
+          <li>
+            <a href="#">Registered Companies</a>
+          </li>
+          <li>
+            <a href="#">Training</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-3 get_in_touch ftr">
+        <h3>GET IN TOUCH</h3>
+        <p>University Hills</p>
+        <p>Kalamessery, Kochi, India</p>
+        <p>83019-79790/221456</p>
+        <a href="mailto:soumen1696@gmail.com">Soumen1696@gmail.com</a>
+      </div>
+      <div class="col-md-3 ftr-logo">
+        <p>Copyright &copy; 2018 CUSAT-SOE | Developed by
+          <a href="http://znumerique.azurewebsites.net" target="_parent">HyperMine Technologies</a>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+  </div>
+</body>
 
 </html>
